a79f7411f452e80040ceca9881fc3bb8bd3bf8a0
g-pg <gabrielpgusso@gmail.com>
2023-03-15 04:14:24 -0300

    visual changes

--- components/Home/HeroSection/HomeHero.jsx
+++ components/Home/HeroSection/HomeHero.jsx
@@ -9,18 +9,16 @@ import Typewriter from "typewriter-effect";
 import MinimizeIcon from "@svg/minimize.svg";
 import MaximizeIcon from "@svg/maximize.svg";
 import CloseIcon from "@svg/close.svg";
-import LnIcon from "@svg/icons/linkedin-icon.svg";
-import GhIcon from "@svg/icons/github-icon.svg";
-import MailIcon from "@svg/icons/email.svg";
 
-import Tape from "../../../public/img/tape3.png";
+import SocialBox from "@components/SocialBox/SocialBox";
+import Tape from "../../../public/img/tape4.png";
 export default function HomeHero() {
 	const [isTyping, setIsTyping] = useState(true);
 	const [minimized, setMinimized] = useState(false);
 	const [maximized, setMaximized] = useState(false);
 
 	return (
-		<section className={styles.section} id="hero-section">
+		<section className={styles.section}>
 			<div className="container">
 				<div className={classNames(styles.sectionWrapper)}>
 					<div className={styles.textCol}>
@@ -29,23 +27,7 @@ export default function HomeHero() {
 						<p className={styles.subText}>
 							_desenvolvedor front-end localizado em Curitiba, PR.
 						</p>
-						<div className={classNames(styles.socialWrapper, "flex-align")}>
-							<a href="#">
-								<LnIcon
-									className={classNames(styles.socialIcon, styles.ln)}
-								/>
-							</a>
-							<a href="#">
-								<GhIcon
-									className={classNames(styles.socialIcon, styles.gh)}
-								/>
-							</a>
-							<a href="#">
-								<MailIcon
-									className={classNames(styles.socialIcon, styles.mail)}
-								/>
-							</a>
-						</div>
+						<SocialBox />
 					</div>
 					<div className={styles.imgCol}>
 						<div
@@ -109,9 +91,9 @@ export default function HomeHero() {
 						</div>
 						<div
 							className={styles.smiley}
-							style={{
-								transform: minimized ? "translate(0, -50%)" : "",
-							}}
+							// style={{
+							// 	transform: minimized ? "translate(0, -50%)" : "",
+							// }}
 						>
 							<Image src={Tape} alt="fita-crepe.png" />
 						</div>
