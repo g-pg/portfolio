9a3e251721d82e8908ef9157098e60ffbd1178f2
g-pg <gabrielpgusso@gmail.com>
2023-03-16 16:17:13 -0300

    move Home to Pages/Home

---
+++ components/Pages/Home/ContactSection/ContactSection.jsx
@@ -0,0 +1,10 @@
+import classNames from "classnames";
+import React from "react";
+
+export default function ContactSection() {
+	return (
+		<section className={classNames("home-section")}>
+			<div className={classNames("container")}>h4</div>
+		</section>
+	);
+}
