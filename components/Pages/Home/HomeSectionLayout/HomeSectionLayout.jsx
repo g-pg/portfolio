b55daece28f0b316a1b165097e25b8b89a92cffb
g-pg <gabrielpgusso@gmail.com>
2023-03-18 04:34:21 -0300

    visual features moved from html to css pseudo el

--- components/Pages/Home/HomeSectionLayout/HomeSectionLayout.jsx
+++ components/Pages/Home/HomeSectionLayout/HomeSectionLayout.jsx
@@ -3,15 +3,17 @@ import { useInView } from "react-intersection-observer";
 import classNames from "classnames";
 import styles from "./HomeSectionLayout.module.css";
 export default function HomeSectionLayout({ children, sectionId, sectionWrapper, sectionTitle }) {
-	const { ref: sectionRef, inView } = useInView({ triggerOnce: true, threshold: 0.1 });
+	const { ref: sectionRef, inView } = useInView({ triggerOnce: true, threshold: 0.05 });
 
 	return (
-		<section
-			className={classNames(styles.homeSection, inView ? styles.inView : styles.notInView)}
-			id={sectionId}
-			ref={sectionRef}
-		>
-			<div className={classNames("container", sectionWrapper)}>
+		<section className={classNames(styles.homeSection)} id={sectionId} ref={sectionRef}>
+			<div
+				className={classNames(
+					"container",
+					sectionWrapper,
+					inView ? styles.inView : styles.notInView
+				)}
+			>
 				{sectionTitle && <h3 className={styles.sectionTitle}>{sectionTitle}</h3>}
 				{children}
 			</div>
