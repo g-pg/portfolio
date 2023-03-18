ee43f84ec8bca74e28f4f383012e7d8cef877bbd
g-pg <gabrielpgusso@gmail.com>
2023-03-18 16:36:27 -0300

    add footer content

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
+import { PrimaryText, HighLight } from "@components/Pages/Home/HomeSectionLayout/HomeSectionLayout";
+import SocialBox from "@components/SocialBox/SocialBox";
 import classNames from "classnames";
 import React from "react";
 import styles from "./Footer.module.css";
 export default function Footer() {
 	return (
 		<footer className={styles.footerWrapper}>
-			<div className={classNames("container", styles.footerWrapper)}></div>
+			<div className={classNames("container", styles.footerWrapper)}>
+				<SocialBox style={{}} iconWidth="25px" />
+			</div>
+			<PrimaryText style={{ fontSize: "0.7rem", width: "auto", marginTop: "-0.5rem" }}>
+				Desenvolvido por mim em <HighLight>NextJS</HighLight> .
+			</PrimaryText>
 		</footer>
 	);
 }
