13f501615d11d4156073e8f78dccb718e7edf7f2
g-pg <gabrielpgusso@gmail.com>
2023-03-19 22:13:57 -0300

    added english in skillset

--- components/Pages/Home/SkillsSection/SkillsSection.jsx
+++ components/Pages/Home/SkillsSection/SkillsSection.jsx
@@ -1,16 +1,17 @@
 import MainCard from "@components/MainCard/MainCard";
 import React, { useRef } from "react";
+
 import GhIcon from "@svg/icons/github-icon.svg";
 import WebPageIcon from "@svg/icons/web-page.svg";
 import ReactIcon from "@svg/icons/react-solid.svg";
-
+import USAFlag from "@svg/icons/usa-flag.svg";
 import styles from "./SkillsSection.module.css";
 import classNames from "classnames";
 import { useInView } from "react-intersection-observer";
 import HomeSectionLayout from "../HomeSectionLayout/HomeSectionLayout";
 
 export default function SkillsSection() {
-	const { ref: wrapperRef, inView } = useInView({ triggerOnce: true });
+	const { ref: cardRef, inView } = useInView({ triggerOnce: true });
 	console.log(inView);
 
 	return (
@@ -19,33 +20,29 @@ export default function SkillsSection() {
 			sectionWrapper={styles.sectionWrapper}
 			sectionTitle={"Skillset"}
 		>
-			<div
-				className={classNames(styles.cardsWrapper, inView && styles.inView)}
-				ref={wrapperRef}
-			>
-				<div className={styles.card}>
-					<MainCard
-						svg={<WebPageIcon />}
-						title={"Linguagens"}
-						text={
-							"Sei criar sites com HTML acessível, CSS responsivo e deixar as coisas dinâmicas com Javascript."
-						}
-					/>
-				</div>
-				<div className={styles.card}>
-					<MainCard
-						svg={<GhIcon />}
-						title={"Versionamento"}
-						text={"Experiência com controle de versão no GitHub."}
-					/>
-				</div>
-				<div className={styles.card}>
-					<MainCard
-						svg={<ReactIcon />}
-						title={"Frameworks e libs"}
-						text={"Conhecimentos em React/NextJS."}
-					/>
-				</div>
+			<div className={classNames(styles.cardsWrapper)}>
+				<MainCard
+					svg={<WebPageIcon />}
+					title={"Linguagens"}
+					text={"Sei criar sites com HTML acessível"}
+				/>
+
+				<MainCard
+					svg={<GhIcon />}
+					title={"Versionamento"}
+					text={"Experiência com controle de versão no GitHub."}
+				/>
+
+				<MainCard
+					svg={<ReactIcon />}
+					title={"Frameworks e libs"}
+					text={"Conhecimentos em React/NextJS."}
+				/>
+				<MainCard
+					svg={<USAFlag />}
+					title={"Inglês"}
+					text={"Leio e escrevo muito bem em inglês."}
+				/>
 			</div>
 		</HomeSectionLayout>
 	);
