00fb71200ee81712dc2f76b0529a893c1ee6dc98
g-pg <gabrielpgusso@gmail.com>
2023-03-19 23:40:10 -0300

    added language card

--- components/Pages/Home/SkillsSection/SkillsSection.jsx
+++ components/Pages/Home/SkillsSection/SkillsSection.jsx
@@ -5,6 +5,8 @@ import GhIcon from "@svg/icons/github-icon.svg";
 import WebPageIcon from "@svg/icons/web-page.svg";
 import ReactIcon from "@svg/icons/react-solid.svg";
 import USAFlag from "@svg/icons/usa-flag.svg";
+
+import SpeechBubble from "@svg/icons/speech-bubble.svg";
 import styles from "./SkillsSection.module.css";
 import classNames from "classnames";
 import { useInView } from "react-intersection-observer";
@@ -24,23 +26,25 @@ export default function SkillsSection() {
 				<MainCard
 					svg={<WebPageIcon />}
 					title={"Linguagens"}
-					text={"Sei criar sites com HTML acessível"}
+					text={
+						"Sei criar sites com HTML acessível, CSS resposivo e deixar as coisas dinâmicas com Javascript."
+					}
 				/>
 
-				<MainCard
+				{/* <MainCard
 					svg={<GhIcon />}
 					title={"Versionamento"}
-					text={"Experiência com controle de versão no GitHub."}
-				/>
+					text={"Controle de versão no GitHub."}
+				/> */}
 
 				<MainCard
 					svg={<ReactIcon />}
 					title={"Frameworks e libs"}
-					text={"Conhecimentos em React/NextJS."}
+					text={"Conhecimentos em React/NextJS, incluindo react-router-dom."}
 				/>
 				<MainCard
-					svg={<USAFlag />}
-					title={"Inglês"}
+					svg={<SpeechBubble />}
+					title={"Idiomas"}
 					text={"Leio e escrevo muito bem em inglês."}
 				/>
 			</div>
