34ea398139218e843d492637f47373964dc1c075
g-pg <gabrielpgusso@gmail.com>
2023-03-21 15:36:51 -0300

    add meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import "../styles/reset.css";
 import "../styles/globals.css";
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32.png" />
+				<meta
+					name="description"
+					content="Desenvolvedor front-end localizado em Curitiba, PR."
+					key="desc"
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta property="og:title" content="Gabriel Gusso | Portfólio" />
+				<meta
+					property="og:description"
+					content="Desenvolvedor front-end localizado em Curitiba, PR."
+				/>
+				<meta property="og:image" content="https://i.ibb.co/GcrGZYX/og-home-img.png" />
+			</Head>
+			<Component {...pageProps} />;
+		</>
+	);
 }
--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,6 @@ export default function Document() {
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-				{/* <link
-					href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;600;700&display=swap"
-					rel="stylesheet"
-				/> */}
 				<link
 					href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;600;700&family=Roboto+Slab:wght@400;500;700;900&display=swap"
 					rel="stylesheet"
