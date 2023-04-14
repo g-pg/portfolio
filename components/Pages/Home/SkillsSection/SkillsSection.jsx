a03a162e9bf5634de628bce461d7808bf10cdfd9
g-pg <gabrielpgusso@gmail.com>
2023-04-14 15:19:42 -0300

    update skills section

--- components/Pages/Home/SkillsSection/SkillsSection.jsx
+++ components/Pages/Home/SkillsSection/SkillsSection.jsx
@@ -1,11 +1,21 @@
 import MainCard from "@components/MainCard/MainCard";
-import React, { useRef } from "react";
+import React from "react";
 
-import GhIcon from "@svg/icons/github-icon.svg";
 import WebPageIcon from "@svg/icons/web-page.svg";
 import ReactIcon from "@svg/icons/react-solid.svg";
-import USAFlag from "@svg/icons/usa-flag.svg";
-
+import {
+	IconCSS,
+	IconGh,
+	IconHtml,
+	IconJS,
+	IconMongo,
+	IconNext,
+	IconNode,
+	IconPrisma,
+	IconReact,
+	IconTS,
+	IconVite,
+} from "@components/ToolsIcons/ToolsIcons";
 import SpeechBubble from "@svg/icons/speech-bubble.svg";
 import styles from "./SkillsSection.module.css";
 import classNames from "classnames";
@@ -13,39 +23,45 @@ import classNames from "classnames";
 import HomeSectionLayout from "../HomeSectionLayout/HomeSectionLayout";
 
 export default function SkillsSection() {
-	// const { ref: cardRef, inView } = useInView({ triggerOnce: true });
-
 	return (
 		<HomeSectionLayout
 			sectionId={"skills"}
 			sectionWrapper={styles.sectionWrapper}
 			sectionTitle={"Skillset"}
 		>
-			<div className={classNames(styles.cardsWrapper)}>
-				<MainCard
-					svg={<WebPageIcon />}
-					title={"Linguagens"}
-					text={
-						"Sei criar sites com HTML acessível, CSS resposivo e deixar as coisas dinâmicas com Javascript."
-					}
-				/>
-
-				{/* <MainCard
-					svg={<GhIcon />}
-					title={"Versionamento"}
-					text={"Controle de versão no Github."}
-				/> */}
-
-				<MainCard
-					svg={<ReactIcon />}
-					title={"Frameworks e libs"}
-					text={"Conhecimentos em React/NextJS, incluindo react-router-dom."}
-				/>
-				<MainCard
-					svg={<SpeechBubble />}
-					title={"Idiomas"}
-					text={"Leio e escrevo muito bem em inglês."}
-				/>
+			<div className={styles.wrapper}>
+				<div className={classNames(styles.cardsWrapper)}>
+					<MainCard
+						svg={<WebPageIcon />}
+						title={"Linguagens"}
+						text={
+							"Sei criar sites com HTML acessível, CSS resposivo e deixar as coisas dinâmicas com Javascript."
+						}
+					/>
+					<MainCard
+						svg={<ReactIcon />}
+						title={"Frameworks e libs"}
+						text={"Conhecimentos em React/NextJS e ferramentas relacionadas."}
+					/>
+					<MainCard
+						svg={<SpeechBubble />}
+						title={"Idiomas"}
+						text={"Leio e escrevo muito bem em inglês."}
+					/>
+				</div>
+				<div className={styles.tools}>
+					<IconHtml />
+					<IconCSS />
+					<IconJS />
+					<IconGh />
+					<IconReact />
+					<IconVite />
+					<IconNext />
+					{/* <IconTS/> */}
+					<IconPrisma />
+					<IconMongo />
+					<IconNode />
+				</div>
 			</div>
 		</HomeSectionLayout>
 	);
