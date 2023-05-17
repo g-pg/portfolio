eabf4713c297226df0523cafef206ff5fa37e2fb
g-pg <gabrielpgusso@gmail.com>
2023-05-17 15:29:52 -0300

    visual changes

--- components/Pages/Home/ProjectsSection/ProjectsSection.jsx
+++ components/Pages/Home/ProjectsSection/ProjectsSection.jsx
@@ -29,56 +29,64 @@ function ProjectCard({
 
 	const [showBtns, setShowBtns] = useState(false); //evita que o usuário clique acidentalmente em mobile
 	return (
-		<>
-			<div
-				className={classNames(
-					styles.projectContainer,
-					inView ? styles.inView : styles.notInView,
-					nickname
-				)}
-				ref={containerRef}
-				onMouseEnter={() => setShowBtns(true)}
-				onMouseLeave={() => setShowBtns(false)}
-			>
-				<Image
-					className={styles.image}
-					alt={name}
-					src={img.desktop}
-					fill="true"
-					object-fit="cover"
-					quality={100}
-				/>
-				<div className={styles.projectText}>
-					<h4 className={styles.projectTitle}>{name}</h4>
-					<p className={styles.projectDesc}>
-						<span>{description}</span>
-					</p>
-				</div>
-				<div
-					className={styles.links}
-					style={{ pointerEvents: showBtns ? "unset" : "none" }}
-				>
-					<Link href={liveLink} target="_blank">
-						<LinkIcon className={styles.linkIcon} />
-					</Link>
-					<Link href={ghLink} target="_blank">
-						<GhIcon className={styles.linkIcon} />
-					</Link>
-				</div>
-				<div className={styles.overlay}></div>
+		<div
+			className={classNames(
+				styles.projectContainer,
+				inView ? styles.inView : styles.notInView,
+				nickname
+			)}
+			ref={containerRef}
+			onMouseEnter={() => setShowBtns(true)}
+			onMouseLeave={() => setShowBtns(false)}
+		>
+			<Image
+				className={styles.image}
+				alt={name}
+				src={img.desktop}
+				fill="true"
+				object-fit="cover"
+				quality={100}
+			/>
+			<div className={styles.projectText}>
+				<h4 className={styles.projectTitle}>{name}</h4>
+				<p className={styles.projectDesc}>
+					<span>{description}</span>
+				</p>
 			</div>
+			<div className={styles.links} style={{ pointerEvents: showBtns ? "unset" : "none" }}>
+				<Link href={liveLink} target="_blank">
+					<LinkIcon className={styles.linkIcon} />
+				</Link>
+				<Link href={ghLink} target="_blank">
+					<GhIcon className={styles.linkIcon} />
+				</Link>
+			</div>
+			<div className={styles.overlay}></div>
 			<style jsx>
 				{`
 					${`.${nickname} {grid-area: ${nickname}}`}
 				`}
 			</style>
-		</>
+		</div>
 	);
 }
 
 export default function ProjectsSection() {
-	//nicknames: "wordle", "malu", "ecommerce", "dropdown", "muvi", "todo", "landing-vue"
-	const projectsToRender = ["wordle", "malu", "ecommerce", "dropdown", "muvi", "landing-vue"];
+	// prettier-ignore
+	const projectsToRender = {
+			'wordle': true,
+			'malu': true,
+			'ecommerce': true,
+			'dropdown': true,
+			'muvi': true,
+			'todo': false,
+			'landing-vue': true,
+			'snek': false,
+			'flepburd': false,
+			'dinosor': true,
+			'jokenpo': true,
+		};
+
 	return (
 		<HomeSectionLayout
 			sectionId="projects"
@@ -87,7 +95,7 @@ export default function ProjectsSection() {
 		>
 			<div className={classNames(styles.projectsWrapper)}>
 				{projects.map((el) => {
-					if (projectsToRender.includes(el.nickname)) {
+					if (projectsToRender[el.nickname]) {
 						return <ProjectCard key={el.id} {...el} />;
 					}
 				})}
