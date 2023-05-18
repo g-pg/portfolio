579a5817b76d838702963b5b00374c5ecd893eea
g-pg <gabrielpgusso@gmail.com>
2023-05-17 21:16:28 -0300

    changed icons position

--- components/Pages/Home/SkillsSection/SkillsSection.jsx
+++ components/Pages/Home/SkillsSection/SkillsSection.jsx
@@ -54,12 +54,12 @@ export default function SkillsSection() {
 					<IconHtml />
 					<IconCSS />
 					<IconJS />
+					<IconTS />
 					<IconGh />
 					<IconReact />
 					<IconNext />
 					<IconVue />
 					<IconVite />
-					<IconTS />
 					<IconPrisma />
 					<IconMongo />
 					<IconNode />
