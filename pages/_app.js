81454c469eb57f3dc6aa1a53f643f56d89707486
Gabriel Gusso <gabrielpgusso@gmail.com>
2023-09-15 00:22:40 -0300

    update meta info

--- pages/_app.js
+++ pages/_app.js
@@ -10,14 +10,14 @@ export default function App({ Component, pageProps }) {
 				<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32.png" />
 				<meta
 					name="description"
-					content="Desenvolvedor front-end localizado em Curitiba, PR."
+					content="Desenvolvedor web localizado em Curitiba, PR."
 					key="desc"
 				/>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta property="og:title" content="Gabriel Gusso | Portfólio" />
 				<meta
 					property="og:description"
-					content="Desenvolvedor front-end localizado em Curitiba, PR."
+					content="Desenvolvedor web localizado em Curitiba, PR."
 				/>
 				<meta property="og:image" content="https://i.ibb.co/GcrGZYX/og-home-img.png" />
 			</Head>
